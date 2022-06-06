83698ac88be8a69128cf6ed634d58f931ea82416
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-06-06 13:44:48 +0100

    access to elements in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def element_at(my_list,idx):
+    if idx < 0:
+        return (None)
+    ex = len(my_list)
+
+    if idx > ex - 1:
+        return (None)
+
+    return (my_list[idx])
+

34fd5fef8bd38033078e60ec407091fa1e8aa4fb
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-05 19:01:33 +0100

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" write to a file"""
+
+
+
+def write_file(filename="", text=""):
+    """write to a file"""
+
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
+    return len(text)

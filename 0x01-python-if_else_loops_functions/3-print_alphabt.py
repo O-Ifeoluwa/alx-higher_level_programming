8c2ff0143ce801a7270562b537549f94e68516a7
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-05 17:36:38 +0100

    conditional statements

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letters in list(map(chr, range(97, 123))):
-    if letters != 101 && 113:
+    if letters != 101 and letters != 113:
         print("{}".format(letters), end='')

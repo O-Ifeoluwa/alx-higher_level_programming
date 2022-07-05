9524aa79a638f2251097320c314933cde13ddbc2
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-05 23:37:43 +0100

    dunno

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,6 +2,8 @@
 """ from JSON string to object"""
 import json
 
+
+
 def from_json_string(my_str):
     """function returns an object repped by a JSON string"""
-    return json.load(my_str)
+    return json.loads(my_str)

94da3dba5dc3e8b6ad1ea1f6b0c92191bce22adb
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-05 15:56:47 +0100

    conditional statements

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 print(number, end=" ")
-if number>0:
-    print(" is positive")
-elif number==0:
-    print(" is zero")
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
 else:
-    print(" is negative")
+    print("is negative")

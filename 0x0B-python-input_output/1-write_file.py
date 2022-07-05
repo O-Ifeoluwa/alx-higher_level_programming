7045353bdc8135530e518a2c737db5b36a0f48fd
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-05 20:10:26 +0100

    append to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,4 +8,4 @@ def write_file(filename="", text=""):
 
     with open(filename, encoding="utf-8") as f:
         f.write(text)
-    return len(text)
+    return (len(text))
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""append to a file function"""
+
+
+def append_write(filename="", text=""):
+    """appends strings to the end of a text file"""
+
+    with open(filename, "a", encoding="utf-8") as f:
+        file.write(text)
+    return (len(text))

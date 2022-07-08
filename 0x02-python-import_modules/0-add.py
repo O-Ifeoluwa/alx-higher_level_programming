da82b81c653e27e707512c2034f3985ad330d803
Op-If <OsatuyiIfe@gmail.com>
2022-07-08 11:24:58 -0700

    import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""program imports definition from add_0.py"""
+from add_0 import add
+
+a = 1
+b = 2
+
+print("{} + {} = {}".format(a, b, add(a, b))
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""import a simple function form a simple file"""
+
+
+def add(a, b):
+    """function adds two numbers"""
+    return(a + b)

32eb2e1a573caa93d4b5006e11349de4df786add
Op-If <OsatuyiIfe@gmail.com>
2022-07-08 11:34:30 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """program imports definition from add_0.py"""
-from add_0 import add
 
-a = 1
-b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b))
+if __name__ = "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a, b, add(a, b))

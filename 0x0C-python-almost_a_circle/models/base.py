7a2d819f459b71f8952ec18805fc62563f5c11f2
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-07-10 18:56:24 +0100

    another incomplete commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,7 +7,7 @@ class Base:
         if id is not None:
             self.id = id
         else:
-            Base.__nb__objects++
+            Base.__nb__objects += 1
 
         self.id = Base.__nb__objects
 

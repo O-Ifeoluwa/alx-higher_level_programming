c652782b835b39ca36ce6ed7b44230b10c5eac98
O-Ifeoluwa <osatuyiIfeoluwa@gmail.com>
2022-08-04 02:27:06 +0100

    sigh

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """prints numbers from 0-99"""
 
-for numbers in range(0, 100):
-    if numbers == 99:
-        print("99")
+for number in range(0, 100):
+    if number == 99:
+        print("{}."format(number))
     else:
-        print("{0:0=2d}, ".format(numbers), end="")
+        print("{0:0=2d}, ".format(number), end="")
